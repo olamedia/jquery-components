bd0dc5f26447778c37bd8feb646522df92a9bd6b
olamedia <olamedia@gmail.com>
2016-05-17 03:34:49 +0500

    Updated

--- js/components/drawer.js
+++ js/components/drawer.js
@@ -4,6 +4,7 @@
 
 	new component('drawer', {
 		'componentConstruct': function(){
+			var self = this;
 			console.log('drawer construct1', self);
 			self.detached = function(){
 				var inst = component.componentInstance(self.codename);
