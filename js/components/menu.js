baa82373419451a427b17b749d6e3a834adaf4de
olamedia <olamedia@gmail.com>
2016-05-23 18:27:12 +0500

    Updated

--- js/components/menu.js
+++ js/components/menu.js
@@ -5,8 +5,10 @@
 
 			var self = this;
 			self.focus = function(){
+				console.log('focus');
 				var $items = self.$e.find('a');
 				if ($items.length){
+					console.log('focus first');
 					$($items.get(0)).focus();
 				}
 				//self.$e.focus();
