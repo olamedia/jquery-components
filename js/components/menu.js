e497657d908607fa809c4f2b4b10347824b13a3f
olamedia <olamedia@gmail.com>
2016-05-23 18:40:02 +0500

    Updated

--- js/components/menu.js
+++ js/components/menu.js
@@ -16,6 +16,36 @@
 			self.$e.on('click', function(){
 				self.focus();
 			});
+			self.keydown = function(e){
+				if (!/(38|40)/.test(e.which) || /input|textarea/i.test(e.target.tagName)){
+					return;
+				}
+				console.log('menu keydown', e.which);
+				e.preventDefault();
+    			e.stopPropagation();
+				var $items = self.$p.find('a');//.not('.menu a');
+				if (!$items.length){
+					return;
+				}
+				var index = $items.index(e.target);
+				if (e.which == 38){ // Up
+					if (index > 0){
+						index--;
+					}
+				}
+				if (e.which == 40){ // Down
+					if (index < $items.length - 1){
+						index++;
+					}
+				}
+				if (!~index) {
+					index = 0
+				}
+				$items.eq(index).focus();
+			}
+			self.$e.on('keydown', function(e){
+				self.keydown(e);
+			});
 		}
 	});
 
