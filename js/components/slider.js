305b861a126566b9d080f733e43e5b7870e3deb5
olamedia <olamedia@gmail.com>
2016-07-07 01:08:52 +0500

    slider.js update

--- js/components/slider.js
+++ js/components/slider.js
@@ -42,7 +42,7 @@
 				$slide.text('Slide #' + i);
 				$slide.css({
 					'left': ((i - index) * self.width) + 'px',
-					'right': ((i - index + 1) * self.width) + 'px'
+					'right': (-(i - index) * self.width) + 'px'
 				});
 			}
 		},
