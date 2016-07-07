0238cb707383e1acef2e3e41af2660ac39e33539
olamedia <olamedia@gmail.com>
2016-07-07 12:32:04 +0500

    article slider js update

--- js/components/articleSlider.js
+++ js/components/articleSlider.js
@@ -1,7 +1,38 @@
 (function(window, $, undef){
 
-	new component('articleSlider', {
+	new component('articleSlider-slide', {
+		'render': function(){
 
+		}
+	});
+
+	new component('articleSlider', {
+		'render': function(){
+			var self = this;
+			self.$headers = $(document.createElement('div'));
+			self.$headers.css({
+				'background': '#00f',
+				'overflow-x': 'hidden',
+				'min-width': '240px',
+				'max-width': '300px',
+			});
+			self.$mediaSlider = $(document.createElement('div'));
+			var $c = self.$e.children();
+			for (var i = 0; i < $c.length; i++){
+				var $slide = $c[i];
+				$slide.detach();
+				$slide.appendTo(self.$headers);
+				//var $e = $(document.createElement('div'));
+				var slideComponent = component('articleSlider-slide').instance();
+				slideComponent.replace('');
+				var $media = $slide.children('articleSlider-media');
+				//var $header = $slide.children('articleSlider-header');
+			}
+			self.$e.append(self.$mediaSlider).append(self.$headers);
+			self.$e.css({
+				'min-height': self.$headers.outerHeight(true) + 'px'
+			});
+		}
 	});
 
 });
