4d16c5632182dc6ea0b4090a77fc3f741ccae37b
olamedia <olamedia@gmail.com>
2016-07-08 01:52:34 +0500

    scrollbar update

--- js/components/scrollbar.js
+++ js/components/scrollbar.js
@@ -10,10 +10,18 @@
 			self.$bar = $(document.createElement('div'));
 			self.$scrollbar.append(self.$bar);
 			self.$e.append(self.$scrollbar);
+			var p = self.$e.css("position");
+			if ('absolute' == p || 'relative' == p){
+			}else{
+				self.$e.css({
+					'position': 'relative'
+				});
+			}
 			//self.$scrollbar.width(self.options.width);
 			self.$scrollbar.addClass('scrollbar');
 			self.$scrollbar.addClass('scrollbar-vertical');
 			self.$scrollbar.css({
+				'position': 'absolute',
 				'top': '0px'
 				//width: self.options.width + 'px',
 				//height: '100%',
